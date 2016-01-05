36cf107185da48977a1cb21d48efa78fed247591
cyx <applekaifa@go.local>
2016-01-05 15:51:08 +0800

    测试
    测试

--- TenMinDemo/TenMinDemo/classes/3/Controller/CYXThreeViewController.m
+++ TenMinDemo/TenMinDemo/classes/3/Controller/CYXThreeViewController.m
@@ -4,7 +4,7 @@
 //
 //  Created by Macx on 15/9/4.
 //  Copyright (c) 2015年 CYX. All rights reserved.
-//
+//  分类控制器
 
 #import "CYXThreeViewController.h"
 
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
     self.view.backgroundColor = [UIColor yellowColor];
 
 }
