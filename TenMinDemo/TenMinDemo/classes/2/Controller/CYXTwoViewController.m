b06a09933f960be0de184f2c9a2249bc166f2cbb
cyx <applekaifa@go.local>
2016-01-05 15:35:28 +0800

    提交测试
    提交测试

--- TenMinDemo/TenMinDemo/classes/2/Controller/CYXTwoViewController.m
+++ TenMinDemo/TenMinDemo/classes/2/Controller/CYXTwoViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor blueColor];
 }
 
 - (void)didReceiveMemoryWarning {
--- TenMinDemo/TenMinDemo/classes/3/Controller/CYXThreeViewController.m
+++ TenMinDemo/TenMinDemo/classes/3/Controller/CYXThreeViewController.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor yellowColor];
+
 }
 
 - (void)didReceiveMemoryWarning {
